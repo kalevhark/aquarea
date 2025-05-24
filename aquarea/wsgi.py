7b963b837c9fcd46d7e1ad95b70335ea96e9e566
kalevhark <Kalev.Hark@mail.ee>
2025-05-24 15:46:36 +0300

    wsgi upd

--- aquarea/wsgi.py
+++ aquarea/wsgi.py
@@ -7,6 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+python_home = '/home/kalev/python/aquarea_env'
+
+import sys
+import site
+
+# Calculate path to site-packages directory.
+
+python_version = '.'.join(map(str, sys.version_info[:2]))
+site_packages = python_home + '/lib/python%s/site-packages' % python_version
+
+# Add the site-packages directory.
+
+site.addsitedir(site_packages)
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +28,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aquarea.settings')
 
 application = get_wsgi_application()
+
